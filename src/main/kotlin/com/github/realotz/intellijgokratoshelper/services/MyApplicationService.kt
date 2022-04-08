3d0e01160a13d3604436d16436bf14623a0c86e8
GitHub Action <action@github.com>
2022-04-08 03:25:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/realotz/intellijgokratoshelper/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.realotz.intellijgokratoshelper.services
+
+import com.github.realotz.intellijgokratoshelper.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/realotz/intellijgokratoshelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/realotz/intellijgokratoshelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.realotz.intellijgokratoshelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.realotz.intellijgokratoshelper.MyBundle
 
 class MyProjectService(project: Project) {
 
